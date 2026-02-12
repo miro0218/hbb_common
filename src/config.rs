fd328e7b25e5ed71f57c6861a3e327be88f7e18f
miro0218 <103075845+miro0218@users.noreply.github.com>
2026-02-12 14:21:53 +0200

    Update rendezvous server and public key constants

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.itsecltd.eu"];
+pub const RS_PUB_KEY: &str = "BjhJ5wmmXKFPMmxF4Jx8VHmRPz0E++14aoyle7P3gZY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
